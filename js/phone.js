fd001c4ef560acb98e3a9fec61cc2aaec7d6f1a6
mohtadahmed <mohtadsaeem13@gmail.com>
2022-09-30 16:36:10 +0600

    phone price increase and decrease

--- js/phone.js
+++ js/phone.js
@@ -2,25 +2,42 @@ function updatePhoneQuentity(isIncrease){
     const phoneInputField = document.getElementById('phone-input-field');
     const phoneInputValueString = phoneInputField.value;
     const phoneInputValue = parseInt(phoneInputValueString);
+
+    let newPhoneNumber;
     
     if(isIncrease === true){
-        const newPhoneNumber = phoneInputValue + 1;
-        phoneInputField.value = newPhoneNumber;
+        newPhoneNumber = phoneInputValue + 1;
     }
     else{
-        const newPhoneNumber = phoneInputValue - 1;
-        phoneInputField.value = newPhoneNumber;
+        newPhoneNumber = phoneInputValue - 1;
     }
-    
+
+    phoneInputField.value = newPhoneNumber;
+    return newPhoneNumber;
+}
+
+function calculatePhonePrice(){
+    const previousPhonePrice = document.getElementById('phone-price');
+    const previousPhonePriceString = previousPhonePrice.innerText;
+    const updatedPhonePrice = parseInt(previousPhonePriceString);
+
+    const phoneNumberValue = document.getElementById('phone-input-field').value;
+    const newPhonePrice = phoneNumberValue * 1219;
+    previousPhonePrice.innerText = newPhonePrice;
 }
 
 
 // Event Handler for increasing the Amount of the Phone Quentity
 document.getElementById('btn-phone-plus').addEventListener('click', function(){
-    updatePhoneQuentity(true);
+    const updatePhoneNumber = updatePhoneQuentity(true);
+    console.log(updatePhoneNumber);
+    // increases the price of the Phone
+    calculatePhonePrice();
 });
 
 // Event Handler for decreasing the Amount of the Phone Quantity
 document.getElementById('btn-phone-minus').addEventListener('click', function(){
   updatePhoneQuentity(false)
+  // decrease the price of the Phone
+  calculatePhonePrice()
 });
